eddc60203d9848ab64fc7278f6f853e59913a958
ogabek <norpulatovogabek42@gmail.com>
2022-07-02 15:32:49 +0500

    task

--- bin/string_length05.dart
+++ bin/string_length05.dart
@@ -7,6 +7,11 @@
     Returns:
         total length of strings
 */
+int func(s1, s2) {
+  return s1.length + s2.length;
+}
+
 void main() {
+  print(func("salom", "mooooolas"));
   // write your code here
 }
